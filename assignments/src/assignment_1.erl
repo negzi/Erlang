dcbe84dcfd04ee47bd3ae39788f7538119523ecf
Negar Fazeli <negar.fazeli@ericsson.com>
2014-09-22 17:28:18 +0200

    Added Function to return even numbers in the list and test cases.

--- assignments/src/assignment_1.erl
+++ assignments/src/assignment_1.erl
@@ -1,5 +1,5 @@
 -module(assignment_1).
--export([math/1, return_odds/1]).
+-export([math/1, return_odds/1, return_evens/1]).
 
 math(List) ->
 	Odds = return_odds(List),
@@ -8,3 +8,5 @@ math(List) ->
 return_odds(List) ->
 	[Odd || Odd <-List, Odd rem 2 =/= 0].
 
+return_evens(List) ->
+	[Even || Even <-List, Even rem 2 =:= 0].
--- assignments/src/assignment_1_tests.erl
+++ assignments/src/assignment_1_tests.erl
@@ -2,7 +2,19 @@
 -include_lib("eunit/include/eunit.hrl").
 
 math_sum_of_odds_test() ->
-	?assertEqual({4}, assignment_1:math([1, 2, 3, 4])).
+    ?assertEqual({4}, assignment_1:math([1, 2, 3, 4])).
 	
 return_odds_test() ->
-	?assertEqual([1, 3], assignment_1:return_odds([1, 2, 3, 4])).
+    ?assertEqual([1, 3], assignment_1:return_odds([1, 2, 3, 4])).
+
+
+return_evens_test() ->
+    ?assertEqual([2, 4], assignment_1:return_evens([1, 2, 3, 4])).
+
+
+return_evens_empty_test() ->
+    ?assertEqual([], assignment_1:return_evens([])).
+
+
+return_odds_empty_test() ->
+    ?assertEqual([], assignment_1:return_odds([])).
