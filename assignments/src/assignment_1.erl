60127f79935e9bbf21f30a2ad480abb5dfe2d0d8
Negar Fazeli <negar.fazeli@ericsson.com>
2014-09-24 11:26:09 +0200

    Added palindrome and unit tests for it.

--- assignments/src/assignment_1.erl
+++ assignments/src/assignment_1.erl
@@ -1,18 +1,22 @@
 -module(assignment_1).
--export([math/1, return_odds/1, return_evens/1,  prod/1]).
+-export([math/1, return_odds/1, return_evens/1,  prod/1, palindrome/1]).
 
 
 math(List) ->
-	Odds = return_odds(List),
-	Evens = return_evens(List),
-	{lists:sum(Odds), prod(Evens)}.
-	
+    Odds = return_odds(List),
+    Evens = return_evens(List),
+    {lists:sum(Odds), prod(Evens)}.
+
 return_odds(List) ->
-	[Odd || Odd <-List, Odd rem 2 =/= 0].
+    [Odd || Odd <-List, Odd rem 2 =/= 0].
 
 return_evens(List) ->
-        [Evens || Evens <-List, Evens rem 2 =:= 0].
+    [Evens || Evens <-List, Evens rem 2 =:= 0].
 
 prod([]) -> 0;
 prod([Head]) -> Head;
 prod([Head|T]) -> Head * prod(T).
+
+
+palindrome(List) ->
+    [Palin || Palin <-List, erlang:is_list(Palin) =:= true , Palin =:= lists:reverse(Palin) ].
--- assignments/src/assignment_1_tests.erl
+++ assignments/src/assignment_1_tests.erl
@@ -2,22 +2,40 @@
 -include_lib("eunit/include/eunit.hrl").
 
 math_sum_of_odds_test() ->
-        ?assertEqual({4, 8}, assignment_1:math([1, 2, 3, 4])).
-	
+    ?assertEqual({4, 8}, assignment_1:math([1, 2, 3, 4])).
+
 return_odds_test() ->
-	?assertEqual([1, 3], assignment_1:return_odds([1, 2, 3, 4])).
+    ?assertEqual([1, 3], assignment_1:return_odds([1, 2, 3, 4])).
+
+return_odds_with_other_types_test() ->
+    ?assertEqual([1, 3], assignment_1:return_odds([1, 2, 3, 4, a])).
 
 return_evens_test() ->
-        ?assertEqual([2, 4], assignment_1:return_evens([1, 2, 3, 4])).
+    ?assertEqual([2, 4], assignment_1:return_evens([1, 2, 3, 4])).
 
 product_test() ->
-	 ?assertEqual(24, assignment_1:prod([1, 2, 3, 4])).
+    ?assertEqual(24, assignment_1:prod([1, 2, 3, 4])).
 
 product_empty_list_test() ->
-         ?assertEqual(0, assignment_1:prod([])).
+    ?assertEqual(0, assignment_1:prod([])).
 
 product_single_list_test() ->
-         ?assertEqual(2, assignment_1:prod([2])).
+    ?assertEqual(2, assignment_1:prod([2])).
 
 product_list_with_ziro_test() ->
-         ?assertEqual(0, assignment_1:prod([2, 0, 1, 3])).
+    ?assertEqual(0, assignment_1:prod([2, 0, 1, 3])).
+
+palindrome_empty_string_test() ->
+    ?assertEqual([[]], assignment_1:palindrome([""])).
+
+palindrome_one_string_test() ->
+    ?assertEqual(["a"], assignment_1:palindrome(["a"])).
+
+palindrome_mixed_strings_test() ->
+    ?assertEqual(["a", "abba", "level", "dad dad"], assignment_1:palindrome(["a", "abba", "neg", "level", "dad dad"])).
+
+palindrome_other_types__test() ->
+    ?assertEqual(["a"], assignment_1:palindrome(["a", 1, abba, {abba}])).
+    
+no_palindrome_with_other_types_test() ->
+    ?assertEqual([], assignment_1:palindrome([1, abba, {abba}])).
