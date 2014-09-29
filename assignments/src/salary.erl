ff513901fe940119c3775fc74f366d299f782a2b
Negar Fazeli <negar.fazeli@ericsson.com>
2014-09-29 15:27:40 +0200

    Added fun to generate employee record.

--- assignments/src/salary.erl
+++ assignments/src/salary.erl
@@ -1,11 +1,25 @@
 -module(salary).
--export([read_lines/1]).
+-export([read_lines/1, fill_employee_info/6]).
+
+-record(employee, {no,
+                  name,
+                  salary,
+                  level,
+                  basesalary,
+                  performance}).
+
 
 read_lines(FileName) ->
     case file:read_file(FileName) of
 	{ok, Data} ->
 	   binary:split(Data, [<<"\n">>], [global]);
 	{error,Reason} ->
-		{error,Reason}
+	    {error,Reason}
     end.
+
+
 		
+fill_employee_info(No, Name, Salary, Level, Basesalary, Performance) ->
+    #employee{no=No, name=Name, salary=Salary, 
+	      level=Level, basesalary=Basesalary, 
+	      performance=Performance}.
--- assignments/src/salary_tests.erl
+++ assignments/src/salary_tests.erl
@@ -1,5 +1,13 @@
+
 -module(salary_tests).
 -include_lib("eunit/include/eunit.hrl").
 
+-record(employee, {no, name, salary, level,  basesalary,performance}).
 wrong_file_name_test() ->
     ?assertEqual({error,enoent}, salary:read_lines("/home/neg/myfil")).
+
+
+assign_employee_info_test() ->
+    ?assertEqual(#employee{no = 1,name = albert, 
+			   salary=21161, level=4, basesalary=15000, performance=bad},
+		 salary:fill_employee_info(1, albert, 21161,4, 15000, bad)).
